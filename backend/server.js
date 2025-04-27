33890cc9e8077f38540d8e1b320c285961567833
iinsue <hinsu93@gmail.com>
2025-04-28 00:54:34 +0900

    add backend routes 폴더

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,12 @@ import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import productRoutes from "./routes/productRoutes.js";
+
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 // allow us to parse the incoming data
 app.use(express.json());
@@ -20,9 +22,7 @@ app.use(helmet());
 // morgan is log the requests to the console
 app.use(morgan("dev"));
 
-app.get("/test", (req, res) => {
-  res.send("Hello from the backend!");
-});
+app.use("/api/products", productRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
