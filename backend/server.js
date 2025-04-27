325d4b630c762066040b1b8063dbfecab99eae42
iinsue <hinsu93@gmail.com>
2025-04-28 00:33:33 +0900

    add dotenv

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,12 @@ import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT;
 
 // allow us to parse the incoming data
 app.use(express.json());
@@ -20,6 +24,6 @@ app.get("/test", (req, res) => {
   res.send("Hello from the backend!");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
