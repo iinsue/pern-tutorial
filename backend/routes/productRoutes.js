7b42cd5b3640a310725afd4519b2c87ca7fc1580
iinsue <hinsu93@gmail.com>
2025-04-29 22:11:00 +0900

    제품 CRUD Controllers 작성

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,13 +2,19 @@ import express from "express";
 
 import {
   createProduct,
-  getAllProducts,
+  deleteProduct,
+  getProduct,
+  getProducts,
+  updateProduct,
 } from "../controllers/productController.js";
 
 const router = express.Router();
 
-router.get("/", getAllProducts);
-
+router.get("/", getProducts);
 router.post("/", createProduct);
+
+router.get("/:id", getProduct);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
 
 export default router;
