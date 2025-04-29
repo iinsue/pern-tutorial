7923cd29a825a80f67586cc3d8491a02bf05a1ad
iinsue <hinsu93@gmail.com>
2025-04-29 23:30:06 +0900

    Arcjet Setup - protecting from bot

---
+++ backend/lib/arcjet.js
@@ -0,0 +1,30 @@
+import arcjet, { tokenBucket, shield, detectBot } from "@arcjet/node";
+
+import "dotenv/config";
+
+// init arcjet
+
+export const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  characteristics: ["ip.src"],
+  rules: [
+    // shield protects your app from common attacks e.g. SQL injection, XSS, CSRF attacks
+    shield({ mode: "LIVE" }),
+    detectBot({
+      mode: "LIVE",
+      // block all bots except search engines
+      allow: [
+        // see the full list a https://arcjet.com/bot-list
+        "CATEGORY:SEARCH_ENGINE",
+      ],
+    }),
+    // rate limiting
+
+    tokenBucket({
+      mode: "LIVE",
+      refillRate: 5,
+      interval: 10,
+      capacity: 10,
+    }),
+  ],
+});
--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import dotenv from "dotenv";
 
 import { sql } from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import { aj } from "./lib/arcjet.js";
 
 dotenv.config();
 
@@ -22,6 +23,41 @@ app.use(helmet());
 
 // morgan is log the requests to the console
 app.use(morgan("dev"));
+
+// apply arcjet rate-limit to all routes
+app.use(async (req, res, next) => {
+  try {
+    const decision = await aj.protect(req, {
+      requested: 1, // specifies that each request consumes 1 token
+    });
+
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        res.status(429).json({ error: "Too Many Requests" });
+      } else if (decision.reason.isBot()) {
+        res.status(403).json({ error: "Bot access denied" });
+      } else {
+        res.status(403).json({ error: "Forbidden" });
+      }
+      return;
+    }
+
+    // check for spoofed bots
+    if (
+      decision.results.some(
+        (result) => result.reason.isBot() && result.reason.isSpoofed()
+      )
+    ) {
+      res.status(403).json({ error: "Spoofed bot detected" });
+      return;
+    }
+
+    next();
+  } catch (error) {
+    console.log("Arcjet Error", error);
+    next(error);
+  }
+});
 
 app.use("/api/products", productRoutes);
 
