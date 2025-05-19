7b75d6d64cea46383800182b3be9cb11b2249ddb
insu <hinsu93@gmail.com>
2025-05-19 17:24:33 +0900

    FE - 상품 리스트 페이지

---
+++ frontend/src/components/ProductCard.jsx
@@ -0,0 +1,42 @@
+import { EditIcon, Trash2Icon } from "lucide-react";
+import React from "react";
+import { Link } from "react-router-dom";
+
+function ProductCard({ product }) {
+  return (
+    <div className="card bg-base-100 shadow-xl transition-shadow duration-300 hover:shadow-2xl">
+      {/* PRODUCT IMAGE */}
+      <figure className="relative pt-[56.25%]">
+        <img
+          src={product.image}
+          alt={product.name}
+          className="absolute top-0 left-0 h-full w-full object-cover"
+        />
+      </figure>
+
+      <div className="card-body">
+        {/* PRODUCT INFO */}
+        <h2 className="card-title text-lg font-semibold">{product.name}</h2>
+        <p className="text-primary text-2xl font-bold">
+          ${Number(product.price).toFixed(2)}
+        </p>
+
+        {/* CARD ACTIONS */}
+        <div className="card-actions mt-4 justify-end">
+          <Link
+            to={`/product/${product.id}`}
+            className="btn btn-sm btn-info btn-outline"
+          >
+            <EditIcon className="size-4" />
+          </Link>
+
+          <button className="btn btn-sm btn-error btn-outline">
+            <Trash2Icon className="size-4" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProductCard;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,45 @@
+import { useEffect } from "react";
+
+import { PlusCircleIcon, RefreshCwIcon } from "lucide-react";
+
+import ProductCard from "../components/ProductCard";
+
+import { useProductStore } from "../store/useProductStore";
+
 function HomePage() {
+  const { products, loading, error, fetchProducts } = useProductStore();
+
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
+
   return (
-    <div>
-      <p>Home Page</p>
-    </div>
+    <main className="mx-auto max-w-6xl px-4 py-8">
+      <div className="mb-8 flex items-center justify-between">
+        <button className="btn btn-primary">
+          <PlusCircleIcon className="mr-2 size-5" />
+          Add Product
+        </button>
+
+        <button className="btn btn-ghost btn-circle" onClick={fetchProducts}>
+          <RefreshCwIcon className="size-5" />
+        </button>
+      </div>
+
+      {error && <div className="alert alert-error mb-8">{error}</div>}
+
+      {loading ? (
+        <div className="flex h-64 items-center justify-center">
+          <div className="loading loading-spinner loading-lg" />
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+          {products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      )}
+    </main>
   );
 }
 
